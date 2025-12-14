7dfd20ae8b58be062bd1187fb3d9a4e1ba7596e3
lecaluizsoccer <lecaluizsoccer@gmail.com>
2025-12-14 17:15:01 +0100

    final version

--- index.js
+++ index.js
@@ -1,42 +1,29 @@
 const mainContainer = document.querySelector(".main-container")
 const body = document.querySelector("body")
 const inputValue = document.querySelector("input")
-// const previewDiv = document.querySelector(".preview")
-// const plusSign = document.querySelector(".plus-sign");
-// const minusSign = document.querySelector(".minus-sign");
+let preview = document.querySelector("#preview")
+
 let watchlist = JSON.parse(localStorage.getItem("watchlist")) || [];
 let currentMovie = null;
 
-body.addEventListener("click", (e) => {
+body.addEventListener("click", async (e) => {
    
     if(e.target.classList.contains("btn")){
-        // previewDiv.style.display = "none"
-        apiMovie(inputValue.value);
-       
+
+        const data = await apiMovie(inputValue.value)
+        if (!data || data.Response === "False") {
+            preview.innerText = "Movie not found!";
+        
+            return;
+        } else {
+            preview.innerText = ""; // clear any previous message
+            mainContainer.innerHTML = renderMovieCards(data, false);
+        }
     }
     if(e.target.classList.contains("my-watchlist")){
         mainContainer.innerHTML = ""
         watchlist.forEach((data) => {
-        mainContainer.innerHTML += `<div class="movie-box">
-        <div class="img-div">
-            <img src="${data.Poster}" alt="">
-        </div>
-        <div class="movie-details">
-            <h2 class="movie-title">${data.Title}<span class="star">★</span> <span class="ratings">${data.Ratings[0].Value}</h2></span>
-            <div class="movieInfo-container">
-                <p class="movie-info">${data.Runtime} ${data.Genre}</p>
-                <div class="watchlist-div">
-                    <p class="minus-sign" data-id="${data.imdbID}">-</p>
-                    <p>Watchlist</p>
-                </div>
-            </div>
-            <p class="movie-description">
-                ${data.Plot}
-            </p>
-        </div>
-        
-    </div>
-    <hr class="line-below">`;
+        mainContainer.innerHTML += renderMovieCards(data);
     })}
 
 
@@ -60,29 +47,11 @@ async function apiMovie(movie) {
 
     const data = await response.json()
 
+    console.log(data.Response)
+
     currentMovie = data;
 
-          mainContainer.innerHTML = `<div class="movie-box">
-                    <div class="img-div">
-                        <img src="${data.Poster}" alt="">
-                    </div>
-                    <div class="movie-details">
-                        <h2 class="movie-title">${data.Title}<span class="star">★</span> <span class="ratings">${data.Ratings[0].Value}</h2></span>
-                        <div class="movieInfo-container">
-                            <p class="movie-info">${data.Runtime} ${data.Genre}</p>
-                            <div class="watchlist-div">
-                                <p class="plus-sign">﹢</p>
-                                <p>Watchlist</p>
-                            </div>
-                        </div>
-                        <p class="movie-description">
-                            ${data.Plot}
-                        </p>
-                    </div>
-                    
-                </div>`
-
-                return data
+    return data
                 
 }
 
@@ -111,30 +80,74 @@ function removeFromWatchlistById(id) {
 
 
 function renderWatchlist(){
-    
     mainContainer.innerHTML = ""
     watchlist.forEach(data => { 
-        mainContainer.innerHTML +=`<div class="movie-box">
-        <div class="img-div">
-            <img src="${data.Poster}" alt="">
-        </div>
-        <div class="movie-details">
-            <h2 class="movie-title">${data.Title}<span class="star">★</span> <span class="ratings">${data.Ratings[0].Value}</h2></span>
-            <div class="movieInfo-container">
-                <p class="movie-info">${data.Runtime} ${data.Genre}</p>
-                <div class="watchlist-div">
-                    <p class="minus-sign" data-id="${data.imdbID}">-</p>
-                    <p>Watchlist</p>
-                </div>
-            </div>
-            <p class="movie-description">
-                ${data.Plot}
-            </p>
-        </div>
-        
-    </div>
-    <hr class="line-below">`;
+        mainContainer.innerHTML += renderMovieCards(data);
 })}
 
+
+
+
+
+// render movies cards and watchlist cards function
+
+function renderMovieCards(data, isWatchlistView=true) {
+    let html = ""
+    if (isWatchlistView) {
+        html = `<div class="movie-box">
+            <div class="img-div">
+                <img src="${data.Poster}" alt="">
+            </div>
+            <div class="movie-details">
+                <h2 class="movie-title">
+                    ${data.Title}
+                    <span class="star">★</span> 
+                    <span class="ratings">${data.Ratings[0].Value}</span>
+                </h2>
+                <div class="movieInfo-container">
+                    <p class="movie-info">${data.Runtime} ${data.Genre}</p>
+                    <div class="watchlist-div">
+                        
+                        <p class="minus-sign" data-id="${data.imdbID}">-</p>
+
+                        <p>Watchlist</p>
+                    </div>
+                </div>
+                <p class="movie-description">
+                    ${data.Plot}
+                </p>
+            </div>
+            
+        </div>
+        <hr class="line-below">`;
+    } else if (isWatchlistView === false) {
+        html = `<div class="movie-box">
+            <div class="img-div">
+                <img src="${data.Poster}" alt="">
+            </div>
+            <div class="movie-details">
+                <h2 class="movie-title">
+                    ${data.Title}
+                    <span class="star">★</span>
+                    <span class="ratings">${data.Ratings[0].Value}</span>
+                </h2>
+
+                <div class="movieInfo-container">
+                    <p class="movie-info">${data.Runtime} ${data.Genre}</p>
+                    <div class="watchlist-div">
+                        <p class="plus-sign">﹢</p>
+                        <p>Watchlist</p>
+                    </div>
+                </div>
+                <p class="movie-description">
+                    ${data.Plot}
+                </p>
+            </div>
+            
+        </div>`;
+    }
+
+    return html
+}
 
 
